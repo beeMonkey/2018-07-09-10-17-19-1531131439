bd4e812744fff03ce9b008676a56318b610ab2f4
beeMonkey <675383749@qq.com>
2018-07-10 14:27:08 +0800

    finish the pratice 1 to 5

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -3,7 +3,9 @@ package practice02;
 public class Person {
     private String name;
     private int age;
+    public Person(){
 
+    }
     Person(String name,int age){
         this.name=name;
         this.age=age;
